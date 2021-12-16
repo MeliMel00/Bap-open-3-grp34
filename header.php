6a6670a5424fa400891945514e3fb74d7682cbbb
Melvin <melvin.chevallier@edu.devinci.fr>
2021-12-16 11:47:32 +0100

    Update Header

---
+++ header.php
@@ -0,0 +1,66 @@
+<header>
+    <nav>
+        <div class="burger">
+            <div class="open">
+                <div class="burger-open">
+                    <a href="#">
+                        <ul></ul>
+                        <ul></ul>
+                        <ul></ul>
+                    </a>
+                </div>
+            </div>
+            <div class="close">
+                <div class="burger-close">
+                    <a href="#"><img src="./img/times-solid.svg" alt="" class="close-div"></a>
+                    <a href="#"><p class="close-div">FERMER</p></a>
+                </div>
+                <a href="#">
+                    <ul>
+                        <p>TITRE</p>
+                    </ul>
+                </a>
+                <a href="#">
+                    <ul>
+                        <p>TITRE</p>
+                    </ul>
+                </a>
+                <a href="#">
+                    <ul>
+                        <p>TITRE</p>
+                    </ul>
+                </a>
+                <a href="#">
+                    <ul>
+                        <p>TITRE</p>
+                    </ul>
+                </a>
+                <a href="#">
+                    <ul>
+                        <p>TITRE</p>
+                    </ul>
+                </a>
+                <a href="#" class="histoire">
+                    <ul>
+                        <p>TITRE</p>
+                        <li class="sub-title">
+                            <p>HELLO</p>
+                        </li>
+                        <li class="sub-title">
+                            <p>HELLO</p>
+                        </li>
+                        <li class="sub-title">
+                            <p>HELLO</p>
+                        </li>
+                    </ul>
+                </a>
+            </div>
+        </div>
+    </nav>
+    <div class="logo-header">
+        <div class="logo"></div>
+    </div>
+    <div class="btn-inscription">
+        <a href="#">inscription</a>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -10,67 +10,10 @@
     <title>Document</title>
 </head>
 
-<header>
-    <div class="burger">
-        <div class="open">
-            <div class="burger-open">
-                <a href="#">
-                    <ul></ul>
-                    <ul></ul>
-                    <ul></ul>
-                </a>
-            </div>
-        </div>
-        <div class="close">
-            <div class="burger-close">
-                <a href="#"><img src="./img/times-solid.svg" alt="" class="close-div"></a>
-                <a href="#"><p class="close-div">FERMER</p></a>
-            </div>
-            <a href="#">
-                <ul>
-                    <p>TITRE</p>
-                </ul>
-            </a>
-            <a href="#">
-                <ul>
-                    <p>TITRE</p>
-                </ul>
-            </a>
-            <a href="#">
-                <ul>
-                    <p>TITRE</p>
-                </ul>
-            </a>
-            <a href="#">
-                <ul>
-                    <p>TITRE</p>
-                </ul>
-            </a>
-            <a href="#">
-                <ul>
-                    <p>TITRE</p>
-                </ul>
-            </a>
-            <a href="#" class="histoire">
-                <ul>
-                    <p>TITRE</p>
-                    <li class="sub-title">
-                        <p>HELLO</p>
-                    </li>
-                    <li class="sub-title">
-                        <p>HELLO</p>
-                    </li>
-                    <li class="sub-title">
-                        <p>HELLO</p>
-                    </li>
-                </ul>
-            </a>
-        </div>
-    </div>
-</header>
+
 
 <body>
-
+<?php include 'header.php'; ?>
     
 
 </body>
