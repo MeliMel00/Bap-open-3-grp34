2b71a2693c25e3c921a8dd5ac76189856f25351c
Corentin <corentin.kistler@edu.devinci.fr>
2022-01-06 01:12:29 +0100

    php

--- code.php
+++ code.php
@@ -1,17 +1,32 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
-    <head>
-        <title>Page protégée par mot de passe</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
     <body>
-        <p>Seul les admin ont accès a cette page</p>
-        <form action="admin.php" method="post">
-            <p>
-            <input type="password" name="mot_de_passe" />
-            <input type="submit" value="Valider" />
-            </p>
-        </form>
+
+    <div class="div-admin">
+
+
+        <p class="page-admin">Seul les admin ont accès a cette page</p>
+        <a class="a-admin" href="index.php">Retour à l'accueil</a>
+            <form action="admin.php" method="post">
+                <p>
+                <input type="password" name="mot_de_passe" placeholder="code d'accès"/>
+                <input type="submit" value="Valider" />
+                </p>
+            </form>
+
+
+    </div>
+        
     </body>
 </html>
